7c03e32f4783c667d1cecd14a086dbf1444b1133
narapark7561 <narapark7561@gmail.com>
2024-08-01 14:28:29 -0600

    tried myself one more time

--- coding challenge.js
+++ coding challenge.js
@@ -36,3 +36,16 @@ TEST DATA 2: [12, 5, -5, 0, 4]
 // - Transform each element to string with ºC
 // - Strings needs to contain day (index + 1)
 // - Add ... between elements and s*/
+
+const temp1 = [17, 21, 23];
+const temp2 = [12, 5, -5, 0, 4];
+
+const showForecast = function (array) {
+  let str = "";
+  for (let i = 0; i < array.length; i++) {
+    str = str + `${array[i]} in ${i + 1} days ...`;
+  }
+  console.log("..." + str);
+};
+
+showForecast(temp1);
