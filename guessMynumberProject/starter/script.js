f37af5d079134c9f05020f14ade8f242ef0eabf0
narapark7561 <narapark7561@gmail.com>
2024-08-05 23:03:32 -0600

    refactoring code to reduce lines

--- guessMynumberProject/starter/script.js
+++ guessMynumberProject/starter/script.js
@@ -61,31 +61,45 @@ document.querySelector('.check').addEventListener('click', function () {
       document.querySelector('.highscore').textContent = highscore;
     }
 
-    // When guess is too high
-  } else if (guess > secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High!🥶';
-      //score = score - 1; 같은거임 밑에랑
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!🌚';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    // When guess is too low
-  } else if (guess < secretNumber) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low!🤖';
-      //score = score - 1; 같은거임 밑에랑
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'You lost the game!🌚';
-      document.querySelector('.score').textContent = 0;
+    //When guess is wrong
+    else if (guess !== secretNumber) {
+      if (score > 1) {
+        document.querySelector('.message').textContent =
+          guess > secretNumber ? 'Too High!🥶' : 'Too Low!🤖';
+        //score = score - 1; 같은거임 밑에랑
+        score--;
+        document.querySelector('.score').textContent = score;
+      } else {
+        document.querySelector('.message').textContent = 'You lost the game!🌚';
+        document.querySelector('.score').textContent = 0;
+      }
     }
   }
 });
+//   // When guess is too high
+// } else if (guess > secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too High!🥶';
+//     //score = score - 1; 같은거임 밑에랑
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = 'You lost the game!🌚';
+//     document.querySelector('.score').textContent = 0;
+//   }
+
+//   // When guess is too low
+// } else if (guess < secretNumber) {
+//   if (score > 1) {
+//     document.querySelector('.message').textContent = 'Too Low!🤖';
+//     //score = score - 1; 같은거임 밑에랑
+//     score--;
+//     document.querySelector('.score').textContent = score;
+//   } else {
+//     document.querySelector('.message').textContent = 'You lost the game!🌚';
+//     document.querySelector('.score').textContent = 0;
+//   }
+// }
 
 ///////////////////////////////////////
 // Coding Challenge #1
