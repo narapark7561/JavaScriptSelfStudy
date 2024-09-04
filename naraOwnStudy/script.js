4ba04d75319dff2367c2fd8b3c56a072b28feef7
narapark7561 <narapark7561@gmail.com>
2024-09-04 11:34:26 -0600

    studied querySelector

--- naraOwnStudy/script.js
+++ naraOwnStudy/script.js
@@ -2,3 +2,6 @@ function changeColor(newColor) {
   const elem = document.getElementById("para");
   elem.style.color = newColor;
 }
+
+const buttonElement = document.querySelector("#love");
+console.log(buttonElement);
