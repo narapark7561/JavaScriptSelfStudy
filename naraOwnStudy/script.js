e0ed102ed3ba35e10e61841e48eacf15c3f4b2f0
narapark7561 <narapark7561@gmail.com>
2024-09-05 09:53:32 -0600

    Study Destructuring with Object

--- naraOwnStudy/script.js
+++ naraOwnStudy/script.js
@@ -1,9 +1,34 @@
-function changeColor(newColor) {
-  const elem = document.getElementById("para");
-  elem.style.color = newColor;
-}
+// function changeColor(newColor) {
+//   const elem = document.getElementById("para");
+//   elem.style.color = newColor;
+// }
 
-const buttonElement = document.querySelector("#love");
-console.log(buttonElement);
+// const buttonElement = document.querySelector("#love");
+// console.log(buttonElement);
 
 //stud Udemy
+
+const restaurant = {
+  name: "Nara Bar",
+  location: "1000 Centre Avenue",
+  mainMenu: ["Steak", "Chicken", "Fish"],
+  dessertMenu: ["Ice cream", "Dessert", "CheeseCake"],
+  order: function system(mainNumber, dessertNumber) {
+    return [this.mainMenu[mainNumber], this.dessertMenu[dessertNumber]];
+  },
+};
+
+// console.log(restaurant);
+
+console.log(restaurant.name);
+
+let [first, second] = restaurant.mainMenu;
+console.log(first, second);
+
+let temp = first;
+first = second;
+second = temp;
+console.log(first, second);
+
+const chaiOrder = restaurant.order(1, 1);
+console.log(chaiOrder);
