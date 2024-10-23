92de55daca79802458c4fe46169621c39945fb61
narapark7561 <narapark7561@gmail.com>
2024-10-23 12:03:00 -0600

    Learn computed properties

--- classAndObject/object2.js
+++ classAndObject/object2.js
@@ -15,7 +15,7 @@ function print(person) {
   console.log(person.age);
 }
 
-const ellie = { name: "ellie", age: 17 };
+const ellie = { name: "Ellie Kim", age: 17 };
 print(ellie);
 
 // with JavaScript magic (dynamically typed language)
@@ -28,3 +28,17 @@ delete ellie.hasJob;
 console.log(ellie.hasJob); // output -> undefined
 
 // 2. Computed properties
+// key should be always 'string' {key : value}
+console.log(ellie.name); // output -> Ellie Kim
+console.log(ellie["name"]); // output -> Ellie Kim  이것도 엘리킴.
+// 두가지 접근방법이다. 이때 주의할 것은 ellie[name]으로 하면 undefined이다.반드시 string으로.
+
+ellie["hasJob"] = true;
+console.log(ellie.hasJob);
+
+function printValue(obj, key) {
+  console.log(obj[key]);
+}
+
+printValue(ellie, "name"); //Ellie Kim
+printValue(ellie, "age"); // 17
